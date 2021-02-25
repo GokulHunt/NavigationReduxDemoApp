618bbc30b44750c1eeefe1dde4f3b952fbf1092e
gokulhunt <gokulavarmanseshadri@gmail.com>
2021-02-25 00:06:34 -0500

    added dynamic filtered list

--- screen/MealDetailsScreen.js
+++ screen/MealDetailsScreen.js
@@ -37,7 +37,7 @@ const MealDetailsScreen = props => {
     props.navigation.setParams({
       toggleFav: toggleFavoriteHandler
     })
-  }, [toggleFavoriteHandler])
+  }, [toggleFavoriteHandler]);
 
 
   return (
--- store/actions/meals.js
+++ store/actions/meals.js
@@ -1,4 +1,4 @@
-export const TOGGLE_FAVORITE = TOGGLE_FAVORITE;
+export const TOGGLE_FAVORITE = "TOGGLE_FAVORITE";
 
 export const toggleFavorite = (id) => {
   return { type: TOGGLE_FAVORITE, mealId: id }
